9c9a4b1a4818fc751bcbf315b9e8c911134096a7
Sparkzz-33 <33409978+Sparkzz-33@users.noreply.github.com>
2019-04-24 19:02:22 +0530

    Update lab8_1.asm

--- lab8_1.asm
+++ lab8_1.asm
@@ -5,12 +5,17 @@ main:
 	li	$s0, 6
 	li	$s1, 0
 	li	$s2, 1
+	li	$t2, 0
 	la	$t0, array
 	j	loop
 loop:
+	bgt	$t2, $s0, exit
 	lw	$t1, 0($t0)
 	bne	$s2, $t1, continue
 	addi	$s1, $s1, 1
 continue:
+	addi 	$t2, $t2, 1
 	addi	$t0, $t0, 4
 	j	loop
+exit:
+	j	$31
